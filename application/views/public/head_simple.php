d140b5ccb838aa9b93507385cdebabec14875338
Juan Palma <juan.palma@me.com>
2021-05-10 11:05:49 -0500

    pixel face en gracias o error

--- application/views/public/head_simple.php
+++ application/views/public/head_simple.php
@@ -40,6 +40,25 @@
 		<meta name="msvalidate.01" content="ED387E3F99B5758EB607324E9928F951" />
 		<meta name="p:domain_verify" content="92115dc4d60becb13618274218b951f2"/>
 		
+		<!-- Facebook Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t,s)}(window, document,'script',
+		'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '167894728587237');
+		fbq('track', 'PageView');
+		</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=167894728587237&ev=PageView&noscript=1"
+		/></noscript>
+		<!-- End Facebook Pixel Code -->
+		
+				
 		
 		<?php if(isset($actual) && $actual !== ''){
 			?>
