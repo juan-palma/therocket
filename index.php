8400a9653b81e3db02d59ff23ab3abb564d36c3e
Juan Palma <juan.a.palma.vidal@gmail.com>
2023-11-23 15:19:14 -0600

    enciroment to production

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $dotenv -> load();
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : getenv('MY_ENVIRONMENT'));
+	define('production', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : getenv('MY_ENVIRONMENT'));
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
